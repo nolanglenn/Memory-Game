e96fa2a2f4c21bd162e8fc344664bfb07f19f1bd
nolanglenn <nolangglenn@gmail.com>
2019-04-28 10:25:11 -0500

    Added navbar

---
+++ memory-game/src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar(props) {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <h1 className="navbar-brand" href="../../..index.html">Navbar</h1>
+            
+        </nav>
+    )
+}
+
+export default Navbar
