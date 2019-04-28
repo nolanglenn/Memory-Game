cd84fba53c17ed6771c38a237381d886529d4b2c
nolanglenn <nolangglenn@gmail.com>
2019-04-28 10:36:56 -0500

    Got navbar items to align properly using bootstraps mx-auto

--- memory-game/src/components/Navbar/index.js
+++ memory-game/src/components/Navbar/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Navbar(props) {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <h1 className="navbar-brand" href="../../..index.html">Navbar</h1>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <h1 className="navbar-brand" href="../../..index.html">Memory Game!</h1>
+            <h1 className="navbar-brand mx-auto" href="../../..index.html">Click an image to begin</h1>
+            <h1 className="navbar-brand" href="../../..index.html">Score: {} | Top score: {}</h1>
             
         </nav>
     )
