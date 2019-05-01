043d974ad51ba35521a289e88be0e43d4bff6136
nolanglenn <nolangglenn@gmail.com>
2019-04-30 22:03:49 -0500

    All characters displayed in divs on page.

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import Navbar from './components/Navbar/index'
 import Jumbotron from './components/Jumbotron';
+import Wrapper from './components/Wrapper'
 import CharacterCard from './components/CharacterCard';
+import characters from './characters.json'
 
 class App extends React.Component {
+
+state = {
+  characters
+}
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <Jumbotron />
-        <CharacterCard />
+        <Wrapper>
+          {this.state.characters.map(character => (
+          <CharacterCard 
+            id={character.id}
+            name={character.name}
+            image={character.image}
+          />
+          )
+          )}
+        </Wrapper>
       </div>
     );
   }
