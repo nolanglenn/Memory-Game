8366997307783a73b4b4b09e7dea64ff3fc085a3
nolanglenn <nolangglenn@gmail.com>
2019-05-04 18:31:55 -0500

    Updates

---
+++ precache-manifest.1e012327d9907fd0de7b54587a9bd2e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b881cb967da8786f1c6b275b5d75510",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "8b5f7621105d266b4847",
+    "url": "/Memory-Game/static/css/main.4e45b332.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Memory-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8b5f7621105d266b4847",
+    "url": "/Memory-Game/static/js/main.d19216e3.chunk.js"
+  },
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "330543c0fe68770704c5d1c6a0927f69",
+    "url": "/Memory-Game/static/media/twin-peaks-mysteries.330543c0.jpg"
+  }
+]);
